245aab57d55c85e3159b20655497e5e5e6f2095a
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-02-14 22:13:31 +0900

    Initial Commit

---
+++ KXAnimatedLabel/KXViewController.h
@@ -0,0 +1,13 @@
+//
+//  KXViewController.h
+//  KXAnimatedLabel
+//
+//  Created by 桜井雄介 on 2014/02/14.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXViewController : UIViewController
+
+@end
---
+++ KXAnimatedLabel/KXViewController.m
@@ -0,0 +1,29 @@
+//
+//  KXViewController.m
+//  KXAnimatedLabel
+//
+//  Created by 桜井雄介 on 2014/02/14.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import "KXViewController.h"
+
+@interface KXViewController ()
+
+@end
+
+@implementation KXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
